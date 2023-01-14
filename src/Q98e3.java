59beb822b79d648994d1f22d58c01a7686a44e1e
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-01-14 13:45:13 +0000

    Add notes on improvements, according to answer, to Q1

--- src/Q98e3.java
+++ src/Q98e3.java
@@ -1,4 +1,8 @@
 public class Q98e3 {
+
+    // This could be improved by using the Ternary Operator
+    // In addition, it would make sense for the next() function to be private, since it is a property of this question
+    //    really, which would not be useful outside this question. We can still make it static.
     public static int next(int x) {
         if (x % 2 == 0) {
             return x / 2;

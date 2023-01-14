1f62452be784f0da895b7adea5212625cf45aa1c
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-01-14 13:45:47 +0000

    Create a utils package with a Pair class

---
+++ src/utils/Pair.java
@@ -0,0 +1,20 @@
+package utils;
+
+public class Pair<A,B> {
+    private final A first;
+    private final B second;
+
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public A first() {
+        return this.first;
+    }
+
+    public B second() {
+        return this.second;
+    }
+
+}
